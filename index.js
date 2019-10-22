e0ff3d81dc75810b5cd2d6ec9ff9fafe027ef202
awais000 <awaismanzoor00@gmail.com>
2019-10-22 14:02:01 +0500

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 80
-
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
 app.post('/api/ipn', (req, res) => {
     console.log(req.body);
     res.send('Hello World!')})
